271471598253990d6c544a565d8b66918773496b
liu9209 <470437289@qq.com>
2019-09-29 14:38:23 +0800

    update:修改

---
+++ spring-cloud-api/src/main/java/org/cloud/api/SpringCloudApiApplication.java
@@ -0,0 +1,16 @@
+package org.cloud.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lin
+ */
+@SpringBootApplication
+public class SpringCloudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApiApplication.class, args);
+	}
+
+}
